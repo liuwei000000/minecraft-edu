c1310b969e3b75db19cf271b508eae151dd1995c
Liu Wei <liuweiky@qq.com>
2016-07-16 22:41:12 +0800

    物品栏功能

--- src/com/huoxingpai/Plugin/EventListener.java
+++ src/com/huoxingpai/Plugin/EventListener.java
@@ -1,14 +1,28 @@
 package com.huoxingpai.Plugin;
 
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Sheep;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 
 public final class EventListener implements Listener {
 	
 	@EventHandler
 	public void onJoin(PlayerJoinEvent event) {
 		event.setJoinMessage(event.getPlayer().getName() + ", 欢迎来到我的世界！");
-	}	
+		
+		PlayerInventory inventory = event.getPlayer().getInventory();
+		
+		ItemStack itemstack = new ItemStack(Material.DIAMOND_SWORD);
+		inventory.addItem(itemstack);
+		
+
+	}
 
 }
